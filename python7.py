310cbfe41caa35179476bbc27168cb441f9801fe
shwetakhobare <shwetakhobare123@gmail.com>
2024-09-05 13:18:11 +0530

    python7 file is added

---
+++ python7.py
@@ -0,0 +1,11 @@
+#Swaping of two numbers
+
+x = int(input("Enter the first number :"))
+y = int(input("Enter the second number :"))
+
+temp = x
+x = y
+y = temp
+
+print("The value of x after swaping is",x)
+print("The value of y aftyer swaping is",y)

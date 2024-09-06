f31dbe8121a827e2fd6d248effcec46608dd574d
shwetakhobare <shwetakhobare123@gmail.com>
2024-09-06 17:06:57 +0530

    python9 file is added

---
+++ python9.py
@@ -0,0 +1,9 @@
+#To convert the kilometers to miles
+
+kilometers = float(input("Enter the value in kilometer :"))
+
+conv_fac = 0.621731
+
+miles = kilometers * conv_fac
+
+print("the %0.2f kilometers in miles are equal %0.2f",(kilometers , miles))

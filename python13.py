a986d3a1a8a746967cb7a13a973786f2ae4280f3
shwetakhobare <shwetakhobare123@gmail.com>
2024-09-09 17:41:40 +0530

    python13 file is added

---
+++ python13.py
@@ -0,0 +1,8 @@
+#to check whether the number is odd or even
+
+num = int(input("Enter the number :"))
+
+if(num%2)==0:
+    print("The number is a even number")
+else:
+    print("The number is a odd number")

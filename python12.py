c16d9e90c1703a98968cb9cb6b0e88fa6bf3f974
shwetakhobare <shwetakhobare123@gmail.com>
2024-09-09 17:40:30 +0530

    python12 file is added

---
+++ python12.py
@@ -0,0 +1,8 @@
+#to check whether the  o is positive negative or zero
+num = int(input("Enter the number"))
+if num > 0:
+    print("It is a positive number")
+elif num == 0:
+    print("It is a zero number")
+else:
+    print("It is a negative number")

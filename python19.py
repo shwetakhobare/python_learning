f41ea159800876d808bf327c4c56e3fac6794788
shwetakhobare <shwetakhobare123@gmail.com>
2024-09-11 16:46:39 +0530

    python19.py file is added

---
+++ python19.py
@@ -0,0 +1,6 @@
+#Display power of 2 using anonymous function
+terms = 10
+result = list(map(lambda x:2 ** x,range(terms)))
+print(" The total terms are :",terms)
+for i in range(terms):
+ print("2 raised to power", i ,"is",result[i])
